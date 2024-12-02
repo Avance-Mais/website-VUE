b68d3d90196bfb7b64bb844fc042048a55ed5e63
nlasvn <114028830+nlasvn@users.noreply.github.com>
2024-12-02 09:36:41 -0300

    feat: adiciona a tela de postagem

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,11 @@ const router = createRouter({
       name: "login",
       component: () => import("../views/LoginView.vue"),
     },
+    {
+      path: "/postagem",
+      name: "postagem",
+      component: () => import("../views/PostagemView.vue"),
+    },
   ],
 });
 
---
+++ src/views/PostagemView.vue
@@ -0,0 +1,76 @@
+<script setup>
+import HeaderComponent from "@/components/HeaderComponent.vue";
+</script>
+
+<template>
+  <div class="layout">
+    <header>
+      <HeaderComponent />
+    </header>
+    <main class="main">
+      <div class="recipiente">
+        <form>
+          <div class="label-input">
+            <label for="titulo">Título</label>
+            <input type="text" name="titulo-postagem" />
+          </div>
+          <div class="label-input">
+            <label for="categoria">Categoria</label>
+            <select name="categoria" id="">
+              <option value="reforco">Reforço Escolar</option>
+              <option value="enem">Enem</option>
+              <option value="concurso">Concurso Públicos</option>
+              <option value="linguagens">Linguagens</option>
+              <option value="vestibulares">Vestibulares</option>
+            </select>
+          </div>
+          <div class="label-input">
+            <label for="postagem">Sobre o que você quer falar?</label>
+            <textarea
+              name="post-descricao"
+              id=""
+              rows="14"
+              class="post-descricao"
+            ></textarea>
+          </div>
+          <input type="submit" class="submit" value="Publicar" />
+        </form>
+      </div>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.main {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 2rem;
+}
+
+.post-descricao {
+  height: 20vh;
+  border: var(--cor-primaria) solid 1px;
+  border-radius: 12px;
+  resize: none;
+}
+
+select {
+  background-color: #ffffff;
+}
+</style>
