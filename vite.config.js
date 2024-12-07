f78757a3eb86e7f68ff9e425c1c1a86bb150f1c3
nlasvn <114028830+nlasvn@users.noreply.github.com>
2024-12-06 21:39:48 -0300

    feat: modifica vite.config para poder ter acesso a api

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,31 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: '/',
+  base: "/",
   server: {
-    host: true, 
+    host: true,
+    proxy: {
+      "/posts": {
+        target: "https://api-avance-plus.onrender.com",
+        changeOrigin: true,
+        secure: true,
+      },
+      "/status": {
+        target: "https://api-avance-plus.onrender.com",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
 });
